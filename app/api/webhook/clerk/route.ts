033637e082b2b50a3aff98ef9da7317daf6409a3
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-05-26 12:33:15 +0300

    update

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,11 +1,10 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
 import prisma from "@/lib/db";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
 import { Svix } from "svix";
+import { Webhook } from "svix";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -14,29 +13,24 @@ export async function POST(req: Request) {
     );
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -50,18 +44,15 @@ export async function POST(req: Request) {
     });
   }
 
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
 
+  ///////   Created
   if (eventType === "user.created") {
     await prisma.user.create({
       data: {
         username: payload.data.username,
-        email: payload.data.email_address[0].email_address,
+        email: payload.data.email_addresses[0].email_address,
         profilePic: payload.data.image_url,
         clerkUserId: payload.data.id,
       },
@@ -75,20 +66,20 @@ export async function POST(req: Request) {
       },
       data: {
         username: payload.data.username,
-        email: payload.data.email_address[0].email_address,
+        email: payload.data.email_addresses[0].email_address,
         profilePic: payload.data.image_url,
       },
     });
   }
 
   if (eventType === "user.deleted") {
-    const existingUser = await prisma.user.findUnique({
+    const exitingUser = await prisma.user.findUnique({
       where: {
         clerkUserId: payload.data.id,
       },
     });
 
-    if (existingUser) {
+    if (exitingUser) {
       await prisma.user.delete({
         where: {
           clerkUserId: payload.data.id,
@@ -96,6 +87,5 @@ export async function POST(req: Request) {
       });
     }
   }
-
   return new Response("", { status: 200 });
 }
