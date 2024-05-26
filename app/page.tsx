667c6abfc3ffd9cb43e46f4206fc37fec7ac9ea0
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-05-26 15:08:41 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex w-full h-full flex-col items-center">
       <div className="container flex flex-col w-full h-full min-h-screen bg-neutral-900 border rounded-sm">
-        Test 26.05
+        Test 26.05 15:00
       </div>
     </main>
   );
